ffcb573186b362d0a233e9d25655f9b40044d75b
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-05-04 03:41:49 -0300

    delete funcional

--- apps/averiguacao/controller_averiguacao/delete_averiguacao_controler/delete_averiguacao_controller.py
+++ apps/averiguacao/controller_averiguacao/delete_averiguacao_controler/delete_averiguacao_controller.py
@@ -3,18 +3,17 @@ import traceback
 from django.http import JsonResponse
 from apps.averiguacao.service_averiguacao.service_delete_averiguacao.delete_averiguacao_service import delete_averiguacao_service
 
-def delete_averiguacao_controller(request):
+def delete_averiguacao_controller(request, averiguacao_id):
     if request.method != 'DELETE':
         return JsonResponse({'erro': 'metodo nao permitido'}, status=405)
+    
     try:
-        if not request.content_type.startswith('multipart'):
-            return JsonResponse({'erro': 'Content-Type deve ser multipart/form-data'}, status=400)
-        data = request.POST
-        arquivos = request.FILES
-        averiguacao = delete_averiguacao_service(data, arquivos)
-        return JsonResponse({'mensagem': 'averiguacao deletada com sucesso', 'id': averiguacao.id})
+        delete_averiguacao_service(averiguacao_id)
+        return JsonResponse({'mensagem': 'Averiguação deletada com sucesso', 'id': averiguacao_id})
+    
     except ValueError as e:
         return JsonResponse({'erro': str(e)}, status=400)
+    
     except Exception as e:
         logging.error(traceback.format_exc())
-        return JsonResponse({'erro': 'erro ao deletar averiguacao', 'detalhes': str(e)}, status=500)
+        return JsonResponse({'erro': 'Erro ao deletar averiguação', 'detalhes': str(e)}, status=500)
--- apps/averiguacao/views_averiguacao/delete_averiguacao_views/averiguacao_delete_views.py
+++ apps/averiguacao/views_averiguacao/delete_averiguacao_views/averiguacao_delete_views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
-from apps.averiguacao.controller_averiguacao.delete_averiguacao_controler import delete_averiguacao_controller
+from apps.averiguacao.controller_averiguacao.delete_averiguacao_controler.delete_averiguacao_controller import delete_averiguacao_controller
 @csrf_exempt
-def delete_averiguacao(request):
-    return delete_averiguacao_controller(request)
+def delete_averiguacao(request,averiguacao_id):
+    return delete_averiguacao_controller(request,averiguacao_id)
