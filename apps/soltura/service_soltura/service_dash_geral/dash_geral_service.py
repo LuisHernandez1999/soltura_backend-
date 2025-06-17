c09bd07f6a049c755a80121f5980fd9d6c52fab0
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-06-17 01:00:24 -0300

    service nova

--- apps/soltura/service_soltura/service_dash_geral/dash_geral_service.py
+++ apps/soltura/service_soltura/service_dash_geral/dash_geral_service.py
@@ -1,98 +1,77 @@
-from ...models.models import Soltura
-from django.utils.timezone import now
+from django.utils import timezone
 from django.db.models import Count
+from ...models.models import Soltura
+
 
 def dash_geral():
-    hoje = now().date()
+    hoje = timezone.localdate()
 
+    garagens = ['PA1', 'PA2', 'PA3', 'PA4']
+    servicos = ['Seletiva', 'Rsu']
 
+    metas = {
+        'Seletiva': {'equipamentos': 45, 'coletores': 36, 'motoristas': 18},
+        'Rsu': {'equipamentos': 45, 'coletores': 178, 'motoristas': 59},
+    }
+
+    # Query para agrupar por garagem e tipo_servico com contagem distinta para evitar duplicatas
     contagem_por_garagem_servico = (
         Soltura.objects
-        .filter(data=hoje, garagem__in=['PA1', 'PA2', 'PA3', 'PA4'], tipo_servico__in=['Rsu', 'Seletiva', 'Remoção'])
+        .filter(
+            data=hoje,
+            garagem__in=garagens,
+            tipo_servico__in=servicos,
+            turno='Diurno'
+        )
         .values('garagem', 'tipo_servico')
         .annotate(
             qtd_veiculo=Count('veiculo'),
             qtd_motorista=Count('motorista'),
             qtd_coletor=Count('coletores'),
+            qtd_equipamento=Count('equipamento'),
         )
     )
 
-    garagens = ['PA1', 'PA2', 'PA3', 'PA4']
-    servicos = ['Rsu', 'Seletiva', 'Remoção']
+    resultados = {}
 
-
-    contagem_garagem = {
-        pa: {
-            'veiculos': 0,
-            'motoristas': 0,
-            'coletores': 0,
-        }
-        for pa in garagens
-    }
-
-    contagem_garagem_servico = {
-        pa: {
-            servico: {
+    # Inicializa o dicionário para todas garagens e serviços
+    for pa in garagens:
+        resultados[pa] = {}
+        for servico in servicos:
+            resultados[pa][servico] = {
                 'veiculos': 0,
                 'motoristas': 0,
-                'coletores': 0
-            } for servico in servicos
-        } for pa in garagens
-    }
+                'coletores': 0,
+                'equipamentos': 0,
+                'meta_batida': {
+                    'motoristas': False,
+                    'coletores': False,
+                    'equipamentos': False
+                }
+            }
 
-
+    # Atualiza resultados com dados da consulta
     for item in contagem_por_garagem_servico:
         garagem = item['garagem']
-        tipo_servico = item['tipo_servico'].lower()
+        tipo_servico = item['tipo_servico']
 
-        contagem_garagem[garagem]['veiculos'] += item['qtd_veiculo']
-        contagem_garagem[garagem]['motoristas'] += item['qtd_motorista']
-        contagem_garagem[garagem]['coletores'] += item['qtd_coletor']
+        # Segurança caso algum serviço que não esteja em metas apareça (ex: erro de dados)
+        if tipo_servico not in metas:
+            continue
 
-        contagem_garagem_servico[garagem][tipo_servico] = {
+        resultados[garagem][tipo_servico] = {
             'veiculos': item['qtd_veiculo'],
             'motoristas': item['qtd_motorista'],
-            'coletores': item['qtd_coletor']
+            'coletores': item['qtd_coletor'],
+            'equipamentos': item['qtd_equipamento'],
+            'meta_batida': {
+                'motoristas': item['qtd_motorista'] >= metas[tipo_servico]['motoristas'],
+                'coletores': item['qtd_coletor'] >= metas[tipo_servico]['coletores'],
+                'equipamentos': item['qtd_equipamento'] >= metas[tipo_servico]['equipamentos'],
+            }
         }
 
-    total_solturas = Soltura.objects.filter(data=hoje).count()
-
-    contagem_servicos = (
-        Soltura.objects
-        .filter(data=hoje, tipo_servico__in=['Rsu', 'Seletiva', 'Remoção'])
-        .values('tipo_servico')
-        .annotate(qtd=Count('id'))
-    )
-    contagem_servico = {item['tipo_servico'].lower(): item['qtd'] for item in contagem_servicos}
-    contagem_rsu = contagem_servico.get('Rsu', 0)
-    contagem_seletiva = contagem_servico.get('Seletiva', 0)
-    contagem_remocao = contagem_servico.get('Remoção', 0)
-
-    contagem_equipamentos = Soltura.objects.filter(data=hoje).count()
-
-    contagem_equipamentos_servicos = (
-        Soltura.objects
-        .filter(data=hoje, tipo_servico__in=['Rsu', 'Seletiva', 'Remoção'])
-        .values('tipo_servico')
-        .annotate(qtd=Count('equipamento'))
-    )
-    equipamentos_por_servico = {item['tipo_servico'].lower(): item['qtd'] for item in contagem_equipamentos_servicos}
-    contagem_equipamentos_rsu = equipamentos_por_servico.get('Rsu', 0)
-    contagem_equipamentos_seletiva = equipamentos_por_servico.get('Seletiva', 0)
-    contagem_equipamentos_remocao = equipamentos_por_servico.get('Remoção', 0)
-
     return {
-        'PA1': contagem_garagem['PA1'],
-        'PA2': contagem_garagem['PA2'],
-        'PA3': contagem_garagem['PA3'],
-        'PA4': contagem_garagem['PA4'],
-        'contagem_por_garagem_e_servico': contagem_garagem_servico,
-        'total_solturas': total_solturas,
-        'contagem_rsu': contagem_rsu,
-        'contagem_seletiva': contagem_seletiva,
-        'contagem_remocao': contagem_remocao,
-        'contagem_equipamentos': contagem_equipamentos,
-        'contagem_equipamentos_rsu': contagem_equipamentos_rsu,
-        'contagem_equipamentos_remocao': contagem_equipamentos_remocao,
-        'contagem_equipamentos_seletiva': contagem_equipamentos_seletiva,
+        'data': hoje,
+        'resultado_por_pa': resultados
     }
